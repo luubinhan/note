eab8bcff53124438c19924db13e2330fc21651b7
anluu1 <anluu1@on.sinch.com>
2026-07-24 10:10:26 +0700

    feat: replace textarea with contentEditable div for note input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 function App() {
   return (
     <main className="min-h-screen bg-slate-950 text-slate-50 flex items-center justify-center p-8">
-      <div className="max-w-lg text-center space-y-4">
-        <textarea
-          autoFocus
-          placeholder=""
-          className="w-full p-2 rounded-md"
-        ></textarea>
+      <div className="min-w-lg min-h-[90vh] max-w-lg text-center space-y-4 ">
+        <div autoFocus className="text-left w-full min-h-[90vh] resize-none text-2xl/12 focus:outline-none" contentEditable="true">fsdaf</div>
       </div>
     </main>
   )
