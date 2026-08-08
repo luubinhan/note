e9aae7bc258f32111c68d49406f123a28b429df0
anluu1 <anluu1@on.sinch.com>
2026-08-08 07:16:50 +0700

    style: adjust padding and text size in App component for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,16 @@ function App() {
   }
 
   return (
-    <main className="min-h-screen text-slate-50 flex items-center justify-center p-8">
+    <main className="min-h-screen text-slate-50 flex items-center justify-center p-10 sm:p-8">
       <Toaster />
       <DeleteNoteButton editorRef={editorRef} />
       <CopyNoteButton editorRef={editorRef} />
-      <div className="min-w-2xl min-h-[90vh] max-w-2xl text-center space-y-4 ">
+      <div className="w-full max-w-2xl min-h-[90vh] text-center space-y-4">
         <div
           ref={editorRef}
           contentEditable
           onPaste={handlePaste}
-          className="text-left w-full min-h-[90vh] resize-none text-4xl/12 focus:outline-none whitespace-pre-wrap break-words"
+          className="text-left w-full min-h-[90vh] resize-none text-2xl/9 sm:text-4xl/12 focus:outline-none whitespace-pre-wrap break-words"
         />
       </div>
     </main>
